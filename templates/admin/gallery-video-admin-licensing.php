737fc0dcd34a2b3502a907411e7030465e4d1192
dianamn <dianamn11@gmail.com>
2018-01-26 19:03:20 +0400

    translation

--- templates/admin/gallery-video-admin-licensing.php
+++ templates/admin/gallery-video-admin-licensing.php
@@ -5,57 +5,57 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 $license = array(
     array(
-        "title" => "Load More And Pagination",
+        "title" => __('Load More And Pagination', 'gallery-video' ),
         "text" => __('This feature will allow to demonstrate only a part of your created videos, hiding the rest of them under “load more” button, or dividing all your projects into several pages.', 'gallery-video' ),
         "icon" => "-250px -465px"
     ),
     array(
-        "title" => "Custom URL For Each Video",
+        "title" => __('Custom URL For Each Video', 'gallery-video' ),
         "text" => __('When adding a Video to gallery, you can add a clickable link from it to a specific page or URL.', 'gallery-video' ),
         "icon" => "-335px -465px"
     ),
     array(
-        "title" => "Advanced View Customization",
+        "title" => __('Advanced View Customization', 'gallery-video' ),
         "text" => __('Unlock all the settings of gallery views, allowing to edit and customize the views, size, effects, buttons, navigation tools, colors and more.', 'gallery-video' ),
         "icon" => "-26px -290px"
     ),
     array(
-        "title" => "Full Video Configuration",
+        "title" => __('Full Video Configuration', 'gallery-video' ),
         "text" => __('Unlock the advanced configuration settings, so that you could use the plugin fully, configure all the corners of videos to your taste.', 'gallery-video' ),
         "icon" => "-132px -295px"
     ),
     array(
-        "title" => "Video Resizer Settings",
+        "title" => __('Video Resizer Settings', 'gallery-video' ),
         "text" => __('Unlock the options allowing to play around videos, thumbs and edit all the corners of media using advanced resizer settings.', 'gallery-video' ),
         "icon" => "-229px -286px"
     ),
     array(
-        "title" => "Color and Text Styling",
+        "title" => __('Color and Text Styling', 'gallery-video' ),
         "text" => __('Unlock more options allowing to edit, add or customize every text and color of the plugin with multiple solutions', 'gallery-video' ),
         "icon" => "-315px -286px"
     ),
     array(
-        "title" => "YouTube Videos",
+        "title" => __('YouTube Videos', 'gallery-video' ),
         "text" => __('Video Gallery can be used with the most popular video site -YouTube, Simply copy the link and add it to the Video Gallery gallery will bring the video in it.', 'gallery-video' ),
         "icon" => "-25px -386px"
     ),
     array(
-        "title" => "Lightbox Views Library",
+        "title" => __('Lightbox Views Library', 'gallery-video' ),
         "text" => __('Some view types of our wonderful Gallery uses quite new designed Lightbox/Popup tool and additional 6 Styles for it.', 'gallery-video' ),
         "icon" => "-141px -383px"
     ),
     array(
-        "title" => "Advanced Lightbox Options",
+        "title" => __('Advanced Lightbox Options', 'gallery-video' ),
         "text" => __('2 Type of Lightbox with tons of social sharing options, zooming, framing, navigation and sliding effects will make users love the plugin.', 'gallery-video' ),
         "icon" => "-243px -394px"
     ),
     array(
-        "title" => "Video slideshow",
+        "title" => __('Video slideshow', 'gallery-video' ),
         "text" => __('Showcase Videos in Stunning Slideshows with advanced options, styles and effects.', 'gallery-video' ),
         "icon" => "-335px -387px"
     ),
     array(
-        "title" => "vimeo Videos",
+        "title" => __('vimeo Videos', 'gallery-video' ),
         "text" => __('The other source of adding videos in Video Gallery- Vimeo. Turn your gallery into Vimeo Gallery using your collection of vimeo videos.', 'gallery-video' ),
         "icon" => "-411px -320px"
     )
